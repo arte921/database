64bc99f2570fffcb6c8c092eb60ad4bd77baf664
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-13 09:44:37 +0200

    initial commit

---
+++ updatedb.js
@@ -0,0 +1,28 @@
+// only indended for manual usage, to introduce or deprecate database enties or when db reorganisation.
+
+const fs = require("fs");
+const path = require("path");
+
+const cwd = process.cwd();
+
+const dbpath = path.join(cwd, "servers.json")
+
+// run this manually or only on first run. MAYBE CONTAIN BUG
+// fs.writeFileSync(path.join(cwd, "temp", "backup.json"), fs.readFileSync(dbpath));
+
+
+
+let database = JSON.parse(fs.readFileSync(dbpath).toString());
+
+for (id in database) {
+    let commandblocklist = database[id].blocklist;
+    database[id].blocklist = {
+        commands:commandblocklist,
+        utils:[]
+    }
+}
+
+
+fs.writeFileSync(dbpath, JSON.stringify(database, null, 4));
+
+//TODO make relative to file location paths
